68a7cfacef17948fd7d02bdc62528ef11e154c37
gldeng <realgldeng@gmail.com>
2025-01-24 15:36:50 +0800

    Update protobuf

--- src/pb/gldeng.substreams.sink.kafka.v1.rs
+++ src/pb/gldeng.substreams.sink.kafka.v1.rs
@@ -1,20 +1,26 @@
 // @generated
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
-pub struct Messages {
+pub struct Batch {
     #[prost(message, repeated, tag="1")]
+    pub topic_bundles: ::prost::alloc::vec::Vec<TopicBundle>,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct TopicBundle {
+    #[prost(string, tag="1")]
+    pub topic: ::prost::alloc::string::String,
+    #[prost(int32, tag="2")]
+    pub schema_id: i32,
+    #[prost(message, repeated, tag="3")]
     pub messages: ::prost::alloc::vec::Vec<Message>,
 }
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct Message {
-    #[prost(string, tag="1")]
-    pub topic: ::prost::alloc::string::String,
-    #[prost(int32, tag="2")]
-    pub schema_id: i32,
-    #[prost(bytes="vec", tag="3")]
+    #[prost(bytes="vec", tag="1")]
     pub key: ::prost::alloc::vec::Vec<u8>,
-    #[prost(bytes="vec", tag="4")]
+    #[prost(bytes="vec", tag="2")]
     pub value: ::prost::alloc::vec::Vec<u8>,
 }
 // @@protoc_insertion_point(module)
---
+++ src/pb/mod.rs
@@ -0,0 +1,4 @@
+
+pub mod kafka {
+    include!("gldeng.substreams.sink.kafka.v1.rs");
+}
